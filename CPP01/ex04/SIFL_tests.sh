60de573f33eb39233158178f03d532ae638a44c5
telli <telli1@hotmail.fr>
2022-09-01 01:07:55 +0200

    CPP01 cleanup and tests added

---
+++ CPP01/ex04/SIFL_tests.sh
@@ -0,0 +1,7 @@
+./SIFL
+./SIFL test test test test
+./SIFL test
+./SIFL test test test
+./SIFL ./Makefile "$" "#"
+./SIFL ./test_files/mirabelle mirabelle glace
+./SIFL ./test_files/lorem_ipsum.txt lorem Laurene
